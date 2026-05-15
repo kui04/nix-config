416734e9d97da2057de8db8076f3b11082b7fa89
kui04 <likuiandmc2004@gmail.com>
2026-05-16 01:21:44 +0800

    secrets(oracle): modify oracle key and refresh binary files

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,7 @@ let
   thinkbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAtj7APUSUfvyTWAGNYT0Amf7qS99lwA6vKEMpNIrn8X";
 
   vultr = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCpz6d04JtnGhHu+xE+h+mANl14pKocGOLvYAnsOXBA";
-  oracle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnWtFuTpNE4XGhrIt5ZT3ESzdhMCmwcyL9HaEBoeIvl";
+  oracle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwafuzUALFT+paueoF1MH3Q0qrI9ubYv0HH4aBgALlc";
 
   proxy-vps = [ vultr oracle ];
 in
